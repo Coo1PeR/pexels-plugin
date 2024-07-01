bb3cb84c951c9564483b72cf0428373a81447ae1
Coo1PeR <coo2per@gmail.com>
2024-07-01 15:15:10 +0300

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from "./components/main-page/main-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    title: 'Main Page',
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  }
+];
