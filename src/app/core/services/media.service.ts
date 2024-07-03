512918b0b8892e7d814147888f2f246f0c59c1c4
Coo1PeR <coo2per@gmail.com>
2024-07-03 20:42:22 +0300

    feat: add media service

---
+++ src/app/core/services/media.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { WorkflowIntegration } from '../../../assets/WorkflowIntegration.node';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MediaService {
+  private PLUGIN_ID = "com.blackmagicdesign.resolve.pexelsplugin";
+
+  initialize() {
+    WorkflowIntegration.Initialize(this.PLUGIN_ID);
+  }
+
+  addVideoToMediaPool(videoPath: string) {
+    const resolve = WorkflowIntegration.GetResolve();
+    const projectManager = resolve.GetProjectManager();
+    const project = projectManager.GetCurrentProject();
+    const mediaPool = project.GetMediaPool();
+    const mediaStorage = resolve.GetMediaStorage();
+
+    let rootBin = mediaPool.GetRootFolder();
+    let pexelsBin = this.getBinByName(mediaPool, rootBin, 'Pexels');
+    if (!pexelsBin) {
+      pexelsBin = this.addBin(mediaPool, rootBin, 'Pexels');
+    }
+
+    mediaPool.SetCurrentFolder(pexelsBin);
+    mediaStorage.AddItemListToMediaPool([videoPath]);
+  }
+
+  private getBinByName(mediaPool, parentBin, binName) {
+    const subFolders = parentBin.GetSubFolderList();
+    for (const folder of subFolders) {
+      if (folder.GetName() === binName) {
+        return folder;
+      }
+    }
+    return null;
+  }
+
+  private addBin(mediaPool, parentBin, binName) {
+    return mediaPool.AddSubFolder(parentBin, binName);
+  }
+}
