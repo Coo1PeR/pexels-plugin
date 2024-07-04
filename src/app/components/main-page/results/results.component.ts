49b852753c25fbe944818972e55dbc702e465697
Coo1PeR <coo2per@gmail.com>
2024-07-04 12:07:26 +0300

    feat: add results

--- src/app/components/main-page/results/results.component.ts
+++ src/app/components/main-page/results/results.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Select, Store} from "@ngxs/store";
+import {VideoState} from "../../../core/stores/state/video.state";
+import {Video} from "../../../core/classes/video";
+import {Observable} from "rxjs";
+import {AsyncPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    AsyncPipe
+  ],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss'
 })
 export class ResultsComponent {
-
+  private store = inject(Store)
+  @Select(VideoState.getVideos) videos$: Observable<Video[]> | undefined;
 }
