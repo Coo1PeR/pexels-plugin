6f276f777a776c7d150c5a3ae7632dee1a753328
Coo1PeR <coo2per@gmail.com>
2024-07-05 11:48:42 +0300

    feat: add selectors

--- src/app/core/stores/state/video.state.ts
+++ src/app/core/stores/state/video.state.ts
@@ -2,16 +2,25 @@ import { State, Action, StateContext, Selector } from '@ngxs/store';
 import { PexelsService } from '../../services/pexels.service';
 import { Injectable } from '@angular/core';
 import { Video } from '../../classes/video';
-import {SearchVideos, VideoAction} from "./video.actions";
+import {LoadVideoToMediaPool, SearchVideos, VideoAction} from "./video.actions";
+//import {MediaService} from "../../services/media.service";
 
 export interface VideoStateModel {
+  page: number;
+  per_page: number;
+  total_results: number;
+  url: string;
   videos: Video[];
-  loading: boolean
+  loading: boolean;
 }
 
 @State<VideoStateModel>({
   name: 'videos',
   defaults: {
+    page: 1,
+    per_page: 9,
+    total_results: 0,
+    url: '',
     videos: [],
     loading: false
   }
@@ -19,7 +28,8 @@ export interface VideoStateModel {
 @Injectable()
 export class VideoState {
   constructor(
-    private pexelsService: PexelsService
+    private pexelsService: PexelsService,
+    //private mediaService: MediaService
   ) {}
 
   @Selector()
@@ -32,12 +42,33 @@ export class VideoState {
     return state.loading;
   }
 
+  @Selector()
+  static getPage(state: VideoStateModel) {
+    return state.page;
+  }
+
+
+  @Selector()
+  static getTotalResults(state: VideoStateModel) {
+    return state.total_results;
+  }
+
+  @Selector()
+  static getUrl(state: VideoStateModel) {
+    return state.url;
+  }
+
   @Action(SearchVideos)
   searchVideos(ctx: StateContext<VideoStateModel>, action: SearchVideos) {
     ctx.patchState({ loading: true });
     return this.pexelsService.searchVideos(action.query).subscribe((result: any) => {
-      const videos = result.videos.map((videoData: any) => new Video(videoData));
-      ctx.patchState({ videos, loading: false });
+      ctx.patchState({
+        videos: result.videos,
+        page: result.page,
+        total_results: result.total_results,
+        url: result.url,
+        loading: false
+      });
     });
   }
 }
