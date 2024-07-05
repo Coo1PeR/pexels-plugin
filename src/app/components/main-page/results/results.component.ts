0c805ce43a66239bde5b045b4d910c09b926b779
Coo1PeR <coo2per@gmail.com>
2024-07-05 11:31:37 +0300

    chore

--- src/app/components/main-page/results/results.component.ts
+++ src/app/components/main-page/results/results.component.ts
@@ -5,6 +5,7 @@ import {Video} from "../../../core/classes/video";
 import {Observable} from "rxjs";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {MatProgressBar} from "@angular/material/progress-bar";
+import {PexelsService} from "../../../core/services/pexels.service";
 
 @Component({
   selector: 'app-results',
@@ -20,7 +21,35 @@ import {MatProgressBar} from "@angular/material/progress-bar";
 })
 export class ResultsComponent {
   private store = inject(Store)
+  private pexelsService = inject(PexelsService);
+  public videoLink: string = ''
+
   @Select(VideoState.getVideos) videos$: Observable<Video[]> | undefined;
   @Select(VideoState.isLoading) loading$: Observable<boolean> | undefined;
+  @Select(VideoState.getPage) page$: Observable<number> | undefined;
+  @Select(VideoState.getTotalResults) totalResults$: Observable<number> | undefined;
+  @Select(VideoState.getUrl) url$: Observable<string> | undefined;
 
+  nextPage() {
+    // if (this.value.trim()) {
+    //   this.store.dispatch(new SearchVideos(this.value));
+    // }
+  }
+
+  downloadVideo(videoId: number): void {
+    const id = videoId.toString();
+    this.pexelsService.getVideoById(id).subscribe(
+      (response) => {
+        if (response) {
+          // @ts-ignore
+          this.videoLink = response.video_files[0].link
+        } else {
+          console.error('Error downloading video');
+        }
+      },
+      (error) => {
+        console.error('Download service error:', error);
+      }
+    );
+  }
 }
