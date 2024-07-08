786abf808dc82a897422aec7722722d86bd5e64b
Coo1PeR <coo2per@gmail.com>
2024-07-08 15:11:41 +0300

    fix: change styling

--- src/app/components/main-page/results/results.component.ts
+++ src/app/components/main-page/results/results.component.ts
@@ -2,13 +2,14 @@ import {Component, inject} from '@angular/core';
 import {Select, Store} from "@ngxs/store";
 import {VideoState} from "../../../core/stores/state/video.state";
 import {Video} from "../../../core/classes/video";
-import {Observable} from "rxjs";
+import {combineLatest, Observable} from "rxjs";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {PexelsService} from "../../../core/services/pexels.service";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconAnchor, MatIconButton} from "@angular/material/button";
 import {MatSuffix} from "@angular/material/form-field";
+import {ApiKeyState} from "../../../core/stores/state/api-key.state";
 
 @Component({
   selector: 'app-results',
@@ -36,6 +37,8 @@ export class ResultsComponent {
   @Select(VideoState.getPage) page$: Observable<number> | undefined;
   @Select(VideoState.getTotalResults) totalResults$: Observable<number> | undefined;
   @Select(VideoState.getUrl) url$: Observable<string> | undefined;
+  @Select(ApiKeyState.getApiKey) apiKey$: Observable<string> | undefined;
+
 
   nextPage() {
     // if (this.value.trim()) {
@@ -43,31 +46,10 @@ export class ResultsComponent {
     // }
   }
 
-  downloadVideo(videoId: number): void {
-    const id = videoId.toString();
-    this.pexelsService.getVideoById(id).subscribe(
-      (response) => {
-        if (response) {
-          // @ts-ignore
-          this.videoLink = response.video_files[0].link;
-          console.log(this.videoLink);
-
-          // Define the file path where the video will be saved
-          const filePath = `/Library/Application Support/Blackmagic Design/DaVinci Resolve/Workflow Integration Plugins/downloads/${videoId}.mp4`;
-
-          // Call the electron function to download the video
-          window.electron.downloadVideo(this.videoLink, filePath).then((filePath) => {
-            console.log(`Video downloaded to: ${filePath}`);
-          }).catch((error) => {
-            console.error('Error downloading video:', error);
-          });
-        } else {
-          console.error('Error downloading video');
-        }
-      },
-      (error) => {
-        console.error('Download service error:', error);
-      }
-    );
+  downloadVideo(videoId: number) {
+    const id = videoId.toString()
+    const apiKey = this.store.selectSnapshot(ApiKeyState.getApiKey);
+    console.log(`Video ID: ${id}, API Key: ${apiKey}`);
+    window.electron.downloadVideo(id, apiKey);
   }
 }
